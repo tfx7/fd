a7a599f6003e8ba2caed2ce4c4a9bf698b8937be
tfx7 <tfx7@live.com>
2017-03-23 16:10:12 -0300

    Create scriptsnew.js

---
+++ scriptsnew.js
@@ -0,0 +1,19 @@
+$(function(){
+    var main_content = $('.main-content_board_index'),
+        logged_in = _userdata["session_logged_in"];
+
+    if( !logged_in ) main_content.eq(4).html('<p class="error-message">Desculpe, conteúdo visível somente a membros do fórum.</p><div style="overflow:hidden"><a href="/login" class="button-link"><i class="fa fa-sign-in"></i> login</a><a href="/register" class="button-link"><i class="fa fa-share-square-o"></i> registre-se</a></div>');
+
+
+    //chatbox
+    function updateIframeSize( x,y )
+    {
+        if (x != '') {
+            $('#' + x).height(y + 10 + 'px');
+        }
+    }
+    
+    if( logged_in ) $('#page-body').prepend('<iframe id="iframe_shoutbox" height="360px" width="100%" frameBorder="0" src="/h3-">Your browser does not support IFRAMEs</iframe>');
+    
+    
+});
